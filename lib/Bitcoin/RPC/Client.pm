c5a0f7633eeb491a232eb42e5e2c32f843c0d756
Wesley Hinds <wesley.hinds@gmail.com>
2020-06-17 16:13:11 -0400

    Use the actual numeric value for IO::Socket::SSL::SSL_VERIFY_NONE rather than exporting it from the module. This prevents adding a required dependency for IO::Socket::SSL which previously was an optional dependency. This fixes #3 (again).

--- lib/Bitcoin/RPC/Client.pm
+++ lib/Bitcoin/RPC/Client.pm
@@ -7,7 +7,6 @@ use warnings;
 
 use Moo;
 use JSON::RPC::Legacy::Client;
-use IO::Socket::SSL qw( SSL_VERIFY_NONE ); 
 
 our $VERSION  = '0.10';
 
@@ -81,7 +80,7 @@ sub AUTOLOAD {
    # For self signed certs
    if ($self->verify_hostname eq 0) {
       $client->ua->ssl_opts( verify_hostname => 0,
-                             SSL_verify_mode => SSL_VERIFY_NONE );
+                             SSL_verify_mode => 0 );
    }
 
    my $obj = {
