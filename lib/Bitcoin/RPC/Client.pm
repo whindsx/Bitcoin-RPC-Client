25a063ee719f5c02d593b94c72cef6b9afe257f7
Wesley Hinds <wesley.hinds@gmail.com>
2023-03-10 09:13:13 -0600

    POD Updates.

--- lib/Bitcoin/RPC/Client.pm
+++ lib/Bitcoin/RPC/Client.pm
@@ -179,10 +179,8 @@ Bitcoin::RPC::Client - Bitcoin Core JSON RPC Client
 =head1 DESCRIPTION
 
 This module implements in Perl the functions that are currently part of the
-Bitcoin Core RPC client calls (bitcoin-cli).The function names and parameters
-are identical between the Bitcoin Core API and this module. This is done for
-consistency so that a developer only has to reference one manual:
-https://developer.bitcoin.org/reference/rpc/index.html
+Bitcoin Core RPC client calls (bitcoin-cli). The function names and parameters
+are identical between the L<Bitcoin Core RPC API|https://developer.bitcoin.org/reference/rpc/index.html> and this module.
 
 =head1 CONSTRUCTOR
 
@@ -202,7 +200,6 @@ This method creates a new C<Bitcoin::RPC::Client> and returns it.
    ssl                 0
    verify_hostname     1
    debug               0
-   syntax              0
 
 cookie - Absolute path to your RPC cookie file (.cookie). When cookie is
 defined user and password will be ignored and the contents of cookie will
@@ -222,9 +219,6 @@ verify_hostname - Disable SSL certificate verification. Needed when
 bitcoind is fronted by a proxy or when using a self-signed certificate.
 
 debug - Turns on raw HTTP request/response output from LWP::UserAgent.
-
-syntax - Removed as of Bitcoin::RPC::Client v0.7, however having the value
-set will not break anything.
 
 =head1 AUTHOR
 
